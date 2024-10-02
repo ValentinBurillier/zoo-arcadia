594eb67ac7c8492890a7144bbc47fd6f80a46c06
Valentin Burillier <valentin.burillier@gmail.com>
2024-10-02 16:10:49 +0200

    add data services

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,16 +1,19 @@
 <?php
 
 namespace App\Controller;
+
+use App\Repository\ServicesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 class HomeController extends AbstractController
 {
   #[Route('/', name: 'app_home')]
-  public function index(): Response
+  public function index(ServicesRepository $servicesRepository): Response
   {
+    $services = $servicesRepository->findAll();
     return $this->render('pages/home.html.twig', [
-      'controller_name' => 'HomeController'
+      'services' => $services
     ]);
   }
 }
