1584e437fd18bbaa08bb6e26f5982b51ed796fd4
Valentin Burillier <valentin.burillier@gmail.com>
2024-10-04 15:20:11 +0200

    pause for fix bug homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ReviewsRepository;
 use App\Repository\ServicesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,13 @@ use Symfony\Component\HttpFoundation\Response;
 class HomeController extends AbstractController
 {
   #[Route('/', name: 'app_home')]
-  public function index(ServicesRepository $servicesRepository): Response
+  public function index(ServicesRepository $servicesRepository, ReviewsRepository $reviewsRepository): Response
   {
+    $reviews = $reviewsRepository->findAll();
     $services = $servicesRepository->findAll();
     return $this->render('pages/home.html.twig', [
-      'services' => $services
+      'services' => $services,
+      'reviews' => $reviews
     ]);
   }
 }
