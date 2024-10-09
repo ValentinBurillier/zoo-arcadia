9f3b7ec08d54dda02c512d65b0885ebd4fde8b38
Valentin Burillier <valentin.burillier@gmail.com>
2024-10-09 09:18:35 +0200

    add stars js on reviews page

---
+++ public/assets/scripts/reviews.js
@@ -0,0 +1,14 @@
+let stars = document.querySelectorAll('.star'); // recup all stars
+let nbstars = stars.length;
+console.log(nbstars);
+stars.forEach((star, index) => { // 
+  star.addEventListener('click', () => {
+    for(let i = 0; i < index; i++) {
+        stars[i].src= "/assets/images/gold_star.webp";
+    }
+    stars[index].src = "/assets/images/gold_star.webp";
+    for(let dif = (index + 1); dif < nbstars; dif++) {
+      stars[dif].src= "/assets/images/zoo-arcadia-white_star.webp";
+    }
+    })
+  });
