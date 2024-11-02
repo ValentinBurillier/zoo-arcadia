34e497416c9a871e833d9d4d28703367b8e230e3
Valentin Burillier <valentin.burillier@gmail.com>
2024-11-02 15:44:37 +0100

    start veterinaire page html & css

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -50,4 +50,42 @@ class UserController extends AbstractController
             'forms' => $forms // Passer les formulaires à la vue
         ]);
     }
+
+    #[Route('/veterinaire', name: 'app_user')]
+    public function veterinaire(ServicesRepository $servicesRepository, ReviewsRepository $reviewsRepository, HabitatRepository $habitatRepository): Response
+    {
+        $reviews = $reviewsRepository
+            ->createQueryBuilder('r')
+            ->andWhere('r.statut = :statut')
+            ->setParameter('statut', 'check')
+            ->getQuery()
+            ->getResult();
+
+        $habitats = $habitatRepository->findAll();
+        $services = $servicesRepository->findAll();
+
+        // Créer un tableau pour stocker les formulaires
+        $forms = [];
+
+        // Créer un formulaire pour chaque animal
+        foreach ($habitats as $habitat) {
+            foreach ($habitat->getAnimals() as $animal) {
+                // Créer une nouvelle instance de Meal
+                $meal = new Meal();
+                $meal->setAnimal($animal); // Associer l'animal
+
+                // Créer le formulaire pour chaque animal
+                $form = $this->createForm(MealType::class, $meal);
+                $forms[$animal->getId()] = $form->createView(); // Stocker la vue du formulaire
+            }
+        }
+
+        return $this->render('user/veterinaire.html.twig', [
+            'habitats' => $habitats,
+            'services' => $services,
+            'reviews' => $reviews,
+            'form' => $form->createView(),
+            'forms' => $forms // Passer les formulaires à la vue
+        ]);
+    }
 }
