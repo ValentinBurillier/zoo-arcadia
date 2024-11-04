a81f6f7d77b691d37ea8e52a200086c4152a821a
Valentin Burillier <valentin.burillier@gmail.com>
2024-11-04 10:20:09 +0100

    end menu

--- public/assets/scripts/menu.js
+++ public/assets/scripts/menu.js
@@ -1,13 +1,40 @@
 let display = false;
 let body = document.querySelector('#body');
-let menuMobil = document.querySelector('section#menuDisplay');
+const menuMobil = document.querySelector('section#menuDisplay');
+let removedMenuMobil;
 let menuIcon = document.querySelector('.img__display');
 let cross = document.querySelector('#menuDisplay img');
+let header = document.querySelector('header');
+
 menuIcon.addEventListener('click', () => {
   menuMobil.attributes.style.value = "";
   body.className = "menuActif";
 })
+
 cross.addEventListener('click', () => {
   menuMobil.attributes.style.value = "display: none";
   body.className = "";
 })
+
+window.addEventListener('resize', () => {
+  let widthNow = window.innerWidth;
+  if (widthNow > 999) {
+    removedMenuMobil = menuMobil;
+    menuMobil.remove();
+  } else if (widthNow < 1000) {
+    if (removedMenuMobil) {
+      header.appendChild(removedMenuMobil);
+    }
+  }
+})
+
+window.addEventListener('DOMContentLoaded', function() {
+  let widthNow = window.innerWidth;
+  remove(menuMobil, widthNow);
+});
+
+function remove(menuMobil, widthNow) {
+  if (widthNow > 999) {
+    menuMobil.remove(); // Supprime le menu si la largeur est supérieure à 999
+  }
+}
