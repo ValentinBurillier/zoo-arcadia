db91fec36e476ccf9c22cde9d8cb89a9aa7b2881
Valentin Burillier <valentin.burillier@gmail.com>
2025-02-01 09:41:51 +0100

    paused & save

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,7 @@ use App\Entity\Meal;
 use App\Form\MealType;
 use App\Entity\Habitat;
 use App\Repository\HabitatRepository;
+use App\Repository\StatutRepository;
 use App\Repository\ReviewsRepository;
 use Doctrine\ORM\EntityManagerInterface; // Importez l'interface ici
 use App\Repository\ServicesRepository;
@@ -18,62 +19,21 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/employe', name: 'app_user')]
-    public function index(ServicesRepository $servicesRepository, ReviewsRepository $reviewsRepository, HabitatRepository $habitatRepository): Response
+    #[Route('/employe', name: 'app_employe')]
+    public function employe(ServicesRepository $servicesRepository, ReviewsRepository $reviewsRepository, StatutRepository $statutRepository): Response
     {
-        $reviews = $reviewsRepository
-            ->createQueryBuilder('r')
-            ->andWhere('r.statut = :statut')
-            ->setParameter('statut', 'check')
-            ->getQuery()
-            ->getResult();
-
-        $habitats = $habitatRepository->findAll();
+        $statusChecked = $statutRepository->findOneBy(['statut' => 'checked']);
+        $idStatusChecked = $statusChecked->getId();
+        $reviews = $reviewsRepository->findBy(['status' => $idStatusChecked]);
+        
         $services = $servicesRepository->findAll();
-
-        // Créer un tableau pour stocker les formulaires
-        $forms = [];
-
-        // Créer un formulaire pour chaque animal
-        foreach ($habitats as $habitat) {
-            foreach ($habitat->getAnimals() as $animal) {
-                // Créer une nouvelle instance de Meal
-                $meal = new Meal();
-                $meal->setAnimal($animal); // Associer l'animal
-
-                // Créer le formulaire pour chaque animal
-                $form = $this->createForm(MealType::class, $meal);
-                $forms[$animal->getId()] = $form->createView(); // Stocker la vue du formulaire
-            }
-        }
-
-        return $this->render('user/index.html.twig', [
-            'habitats' => $habitats,
-            'services' => $services,
-            'reviews' => $reviews,
-            'form' => $form->createView(),
-            'forms' => $forms // Passer les formulaires à la vue
-        ]);
+        dd($services);
+        return $this->render('user/employe.html.twig');
     }
 
     #[Route('/veterinaire', name: 'app_user')]
-    public function veterinaire(Request $request, HabitatRepository $habitatRepository, EntityManagerInterface $entityManager): Response
+    public function veterinaire(): Response
     {
-        $habitats = $entityManager->getRepository(Habitat::class)->findAll();
-$forms = [];
-
-// Créer les formulaires pour chaque animal
-foreach ($habitats as $habitat) {
-    foreach ($habitat->getAnimals() as $animal) {
-        $form = $this->createForm(AnimalType::class, $animal);
-        $forms[$animal->getId()] = $form->createView(); // Stocke le formulaire par ID
-    }
-}
-
-
-        return $this->render('user/veterinaire.html.twig', [
-            'forms' => $form->createView(),
-            'habitats' => $habitats,
-        ]);
+        return $this->render('user/veterinaire.html.twig');
     }
 }
