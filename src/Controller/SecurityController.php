cade7cda6ebf2662cf58d2dce1a46d4affaa22c0
Valentin Burillier <valentin.burillier@gmail.com>
2025-02-17 16:30:53 +0100

    end review login page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,36 +8,23 @@ use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login(Request $request, EntityManagerInterface $em)
+    public function login(Request $request, EntityManagerInterface $em, AuthenticationUtils $authenticationUtils)
     {
-        $user = new User();
-  
-        $form = $this->createForm(LoginType::class, $user);
-        $form->handleRequest($request);
-        if($form->isSubmitted()) {
-            if($form->isValid()) {
-                $email = $user->getEmail();
-                $password = $user->getPassword();
-                $getUser = $em->getRepository(User::class)->findOneBy(['email' => $email]);
-                if(isset($getUser)) {
-                    if (password_verify($password, $getUser->getPassword())) {
-                        return $this->redirectToRoute('app_home');
-                    } else {
-                        $this->addFlash('error', 'Mot de passe incorrect.');
-                    }
-                } else {
-                    $this->addFlash('error', 'Utilisateur non trouvé.');
-                }
-            } else {
-                $this->addFlash('error', 'Formulaire invalide.');
-            }
-        }
-        return $this->render('security/index.html.twig', [
-            'form' => $form->createView()
+        // Récupérer les erreurs de connexion (si existantes)
+        $error = $authenticationUtils->getLastAuthenticationError();
+        
+        // Récupérer le dernier email saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
 
